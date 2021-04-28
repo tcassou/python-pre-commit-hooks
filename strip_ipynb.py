1bfe9f721e1c8d825a77000303a8291e410cd200
Thomas Cassou <thomas.cassou@pennylane.tech>
2021-04-28 10:54:52 +0200

    update ipynb hooks

--- strip_ipynb.py
+++ strip_ipynb.py
@@ -7,23 +7,24 @@ files = sys.argv[1:]
 
 def strip_output_from_cell(cell):
     """
-    Takes a notebook cell and removes the "prompt_number" field
-    and the "outputs" field.
+    Remove variable fields such as outputs, prompt_number/execution_count from an IPython notebook.
+    This allows a cleaner versioning with diffs only showing modifications of input cells.
     """
     if 'outputs' in cell:
         cell['outputs'] = []
     if 'prompt_number' in cell:
         del cell['prompt_number']
+    if 'execution_count' in cell:
+        cell['execution_count'] = None
 
 
 def clean_notebook(file):
     notebook = json.load(open(file, 'r'))
-    cells = notebook['worksheets'][0][
-        'cells'] if 'worksheets' in notebook else notebook['cells']
+    cells = notebook['worksheets'][0]['cells'] if 'worksheets' in notebook else notebook['cells']
     for cell in cells:
         strip_output_from_cell(cell)
-    json.dump(notebook, open(file, 'w'), sort_keys=True,
-              indent=1, separators=(',', ': '))
+    json.dump(notebook, open(file, 'w'), sort_keys=True, indent=2, separators=(',', ': '))
+
 
 for file in files:
     clean_notebook(file)
